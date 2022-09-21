fb2005d5fa7443ba3a428109185a8773de2b67f1
hani235 <68960475+hani235@users.noreply.github.com>
2022-09-21 13:01:15 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  public field: number[] = [1,2,3,4,5,6,7,8,9];
+  player;
+  playerX = 'X';
+  playerO = 'O';
+
+  ngOnInit() {
+
+  }
+
+
+  public getNumber (number : number){
+
+    if(player ){
+
+    }
+
+  }
+
+  public isEven(){number : number}{
+
+    
+
+  }
+
 }
+
+
+
+
+
+
